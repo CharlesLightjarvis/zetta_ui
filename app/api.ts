3fdebfcc61119c101e9a819ffbc7c9b3f9de690f
CharlesLightjarvis <118352168+CharlesLightjarvis@users.noreply.github.com>
2025-03-22 14:34:11 +0100

    reverb works in production

--- app/api.ts
+++ app/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://zetta.test/api/v1", // LOCAL
+  // baseURL: "https://api.talkenoo.click/api/v1", // PROD
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
